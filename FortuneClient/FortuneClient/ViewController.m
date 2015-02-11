74d99ae19c1149285f28f684df189a890d212d3d
Oleksii Kozulin <Jlexyc@admins-MacBook-Pro.local>
2015-02-11 12:55:04 +0200

    - testProject

---
+++ FortuneClient/FortuneClient/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FortuneClient
+//
+//  Created by Oleksii Kozulin on 2/11/15.
+//  Copyright (c) 2015 Oleksii Kozulin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
